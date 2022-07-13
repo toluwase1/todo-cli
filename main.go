2e5a0d173c5196879b561ec7ec7a60ddc08746d9
toluwase <toluwasethomas1@gmail.com>
2022-07-13 08:15:43 +0100

    create and delete method

--- main.go
+++ main.go
@@ -1,1 +1,33 @@
 package main
+
+var todolist []string
+
+func main() {
+
+}
+
+func createTodo(todolist []string) []string {
+	for i := 0; i < len(todolist); i++ {
+		todolist = append(todolist, todolist[i])
+	}
+	return todolist
+}
+
+func deleteTodo(todolist []string, value int) {
+	for i := 0; i < len(todolist); i++ {
+		if i == value {
+			left := todolist[:value+1]
+			right := todolist[value:]
+			todolist = append(todolist, left...)
+			todolist = append(todolist, right...)
+		}
+	}
+}
+
+func updateTodo() {
+
+}
+
+func ListAllTodo() {
+
+}
